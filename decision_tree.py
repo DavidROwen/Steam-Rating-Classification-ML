92a0923e8ebaf924cf1744eb04f50147b70aeb0a
David Owen <david.owen.dev@gmail.com>
2019-12-06 13:47:47 -0700

    Added to and optimized decision tree code

--- decision_tree.py
+++ decision_tree.py
@@ -3,7 +3,7 @@ from arff import Arff
 from sklearn import tree
 import graphviz 
 
-def sk_learn(data="goodGames.arff"):
+def sk_learn(data="default.arff"):
     folds = 10
     mat = Arff(data, label_count=1)
 
@@ -15,8 +15,10 @@ def sk_learn(data="goodGames.arff"):
     splits = np.array_split(mat.data, folds)
     
     Acc = 0
+    min_split = 300
+    print("Minsplit: {}".format(min_split))
     for f in range(folds):
-        print("Fold {}:".format(f))
+        # print("Fold {}:".format(f))
         train = np.array([])
         for other in range(folds):
             if train.size == 0 and other != f:
@@ -27,25 +29,29 @@ def sk_learn(data="goodGames.arff"):
         data = train[:,0:-1]
         labels = train[:,-1].reshape(-1,1)
 
-        clf = tree.DecisionTreeClassifier()
+
+        clf = tree.DecisionTreeClassifier(criterion="entropy", min_samples_split=min_split, min_samples_leaf=25)
         clf = clf.fit(data, labels)
         pred = clf.predict(data)
         new_acc = score(pred, labels)
-        print("\tTrain Acc {}".format(new_acc))
+        # print("\tTrain Acc {}".format(new_acc))
 
         data2 = splits[f][:,0:-1]
         labels2 = splits[f][:,-1].reshape(-1,1)
         pred = clf.predict(data2)
         new_acc = score(pred,labels2)
-        print("\tTest Acc {}".format(new_acc))
+        # print("\tTest Acc {}".format(new_acc))
         Acc += new_acc
 
     Acc = Acc / folds
-    print("Accuracy = [{:.2f}]".format(Acc))
+    print("Accuracy = [{:.4f}]".format(Acc))
 
-    dot_data = tree.export_graphviz(clf, out_file=None, feature_names=mat.get_attr_names()[:-1], max_depth=5, class_names = True)     
-    graph = graphviz.Source(dot_data) 
-    graph.render("wave") 
+    # classes =["Overwhelmingly_Positive", "Very_Positive", "Positive", "Mostly_Positive", "Mixed", "Mostly_Negative", "Negative", "Very_Negative", "Overwhelmingly_Negative"]
+    # dot_data = tree.export_graphviz(clf, out_file=None, feature_names=mat.get_attr_names()[:-1], class_names=classes, max_depth=6, filled=True, rounded=True)     
+    # graph = graphviz.Source(dot_data) 
+    # graph.render("wave") 
+
+    return Acc
 
 def score(pred, labels):
     new_acc = 0
@@ -60,4 +66,8 @@ def score(pred, labels):
             new_acc += 1
     return new_acc / len(pred)
 
-sk_learn()
+total_acc = 0
+rounds = 4
+for _ in range(rounds):
+    total_acc += sk_learn()
+print("Accuracy = [{:.4f}]".format(total_acc / rounds))
--- label_reducer.py
+++ label_reducer.py
@@ -11,11 +11,11 @@
 #     Overwhelmingly_Negative
 # }
 
-def remove(labels, old, new):
+def remove_labels(labels, old, new):
 
     with open(old) as oldfile, open(new, 'w') as newfile:
         for line in oldfile:
             if not any(bad_label in line for bad_label in labels):
                 newfile.write(line)
 
-remove(["Mixed", "Mostly_Negative", "Negative", "Very_Negative", "Overwhelmingly_Negative"], "default.arff", "goodGames.arff")
+remove_labels(["Negative", "Positive"], "default.arff", "oldgames.arff")
